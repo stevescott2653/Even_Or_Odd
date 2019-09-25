8b8c64877254193e070fc4d3bdc05182f46dcbaf
Steven Scott <stevescott2653@gmail.com>
2019-09-25 14:17:22 -0400

    Add files via upload

---
+++ Even_Or_Odd/Even_Or_Odd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("It is an even number");
+            }
+            else
+            {
+                Console.WriteLine("It is an odd number");
+            }
+        }
+    }
+}
